6801af3c5fdb49c2759ac695d8690fb26c35bb21
Will Lee <willlee19951009@gmail.com>
2021-12-13 18:16:43 +0800

    cookie secure

--- src/validators/task-validators.js
+++ src/validators/task-validators.js
@@ -3,11 +3,11 @@ import {
     Task
   } from "../models";
 
-const title = check("title").not().isEmpty().withMessage("The title should not be empty");
-const status = check("status", "status is required").not().isEmpty();
-const startDate = check("startDate", "startDate is required").not().isEmpty();
-const location = check("location", "location is required").not().isEmpty();
-const issuer = check("issuer", "issuer is required").not().isEmpty();
+//const title = check("title").not().isEmpty().withMessage("The title should not be empty");
+//const status = check("status", "status is required").not().isEmpty();
+//const startDate = check("startDate", "startDate is required").not().isEmpty();
+//const location = check("location", "location is required").not().isEmpty();
+//const issuer = check("issuer", "issuer is required").not().isEmpty();
 
 
 export const taskValidations = [title, status, startDate, location, issuer];
