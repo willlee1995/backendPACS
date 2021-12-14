4748b381c0485944203a3d2c6cf107d9987622df
Will Lee <willlee19951009@gmail.com>
2021-12-14 23:49:39 +0800

    prod

--- src/index.js
+++ src/index.js
@@ -40,7 +40,14 @@ app.use(passport.initialize())
 app.use((req,res,next)=> {
     res.setHeader(
         'Access-Control-Allow-Origin', 'https://taskmanager-one.vercel.app/'
+        
     )
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      )
     next()
 })
 app.use('/users', userApis)
