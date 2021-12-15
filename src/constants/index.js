87e361af27e05774e33c344338f7d856bd75ae29
Will Lee <willlee19951009@gmail.com>
2021-12-15 20:42:18 +0800

    deploy

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,4 +6,3 @@ export const DOMAIN = process.env.APP_DOMAIN;
 export const PORT = process.env.PORT;
 export const SECRET = process.env.APP_SECRET;
 export const DB = process.env.APP_DB;
-export const SENDGRID_API = process.env.APP_SENDGRID_API;
